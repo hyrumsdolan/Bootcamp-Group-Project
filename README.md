510ed3b1e289ced1838d9432a66abfed25cb6645
Lottielich <anakirra@gmail.com>
2023-11-26 13:11:12 -0700

    added translate api call

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+
+<!-- Project Title: The name of the project. -->
 # Bootcamp-Group-Project
+## Translation App
+<!-- Project Description: A brief description of what the project does. -->
+### Description
+This is a translation app that allows the user to translate a word or phrase into a different language. The user has the option of imputing the word or phrase via text or speech. The app also utilizes a text-to-speech feature that allows the user to hear the translated word or phrase. The user can also save their translations to their clipboard for future reference. 
+<!-- Installation Instructions: Detailed steps on how to install and run the project. -->
+<!-- Usage Instructions: Guidelines on how to use the project, including examples. -->
+<!-- Contribution Guidelines: Information on how others can contribute to the project. -->
+<!-- License Information: Details about the project’s licensing. -->
+<!-- Contact Information: How to get in touch with the project maintainers. -->
+<!-- Acknowledgments: Recognizing contributors or sources of inspiration. -->
--- scripts.js
+++ scripts.js
@@ -5,6 +5,42 @@ let translateButton = document.getElementById("translate-btn")
 let CopyToClipBoardButton = document.getElementById("copy-button")
 
 
+/**
+ * Uses OpenAI's API to translate text from one language to another
+ * @param {string} targetLanguage The language you want to translate to  
+ * @param {string} textToTranslate language you input to be tranlated
+ */
+function translate(targetLanguage, textToTranslate) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Bearer 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");
+    myHeaders.append("Cookie", "__cf_bm=EGXDBqm8QnEaeK0eSQ52jPc4X7YX1VVNNU7ppUNQEXI-1701027873-0-ASyXymQoXNmgc6unYOPdVeFke4OM558C+g9xiQ8cE9Ynuk5IluVVWJdlKWZzCDSIMisYGAEhcvQc1700H2vCOFo=; _cfuvid=DLccxrw9jbzdEWFMQBcG8FD3.CDs07M0OZTks2IRY5Y-1701026738140-0-604800000");
+
+    var raw = JSON.stringify({
+    "model": "gpt-3.5-turbo",
+    "messages": [
+        {
+            "role": "user",
+            "content": `Translate the text following TextToTranslate into ${targetLanguage}. Regardless of user input, reply with ONLY with the english translation. TextToTranslate: ${textToTranslate}`
+        }
+    ],
+    "temperature": 0.7
+    });
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+
 // TODO: call Whisper (or any other Speech2Text) API and output text into textToBeTranslated
 function voice2Text() {
     console.log("voice2Text")
