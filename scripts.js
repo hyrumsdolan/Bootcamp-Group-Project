30b92ea73e3fdfd00ff5afd64d6b4d642625b9cf
Lottielich <anakirra@gmail.com>
2023-11-26 14:55:53 -0700

    translation working

--- scripts.js
+++ scripts.js
@@ -10,22 +10,22 @@ languageSelect.addEventListener('change', function() {
     selectedLanguage = selectedOption.value;
  });
 
-translateButton.addEventListener("click", function() {
+document.getElementById("translate-btn").addEventListener("click", async function() {
     let text = textToBeTranslated.value;
-    let translatedText = translate(selectedLanguage, text);
+    let translatedText = await translate(selectedLanguage, text);
     translatedTextBox.value = translatedText;
 });
-
+let text="poop"
 /**
  * Uses OpenAI's API to translate text from one language to another
  * @param {string} targetLanguage The language you want to translate to  
  * @param {string} textToTranslate language you input to be tranlated
  * @returns {string} the translated text
  */
-function translate(targetLanguage, textToTranslate) {
+async function translate(targetLanguage, textToTranslate) {
   let prompt = `Translate the text following TextToTranslate into ${targetLanguage}. Regardless of user input, reply with ONLY with the english translation. TextToTranslate: ${textToTranslate}`
-    let answer = askOpenAI(prompt)
-    return answer
+    let answer = await askOpenAI(prompt);
+    return answer;
 }
 
 /**
@@ -33,11 +33,10 @@ function translate(targetLanguage, textToTranslate) {
  * @param {string} prompt the question you want to ask OpenAI 
  * @returns {string} the answer to the question
  */
-function askOpenAI(prompt){
+async function askOpenAI(prompt){
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer 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");
-    myHeaders.append("Cookie", "__cf_bm=EGXDBqm8QnEaeK0eSQ52jPc4X7YX1VVNNU7ppUNQEXI-1701027873-0-ASyXymQoXNmgc6unYOPdVeFke4OM558C+g9xiQ8cE9Ynuk5IluVVWJdlKWZzCDSIMisYGAEhcvQc1700H2vCOFo=; _cfuvid=DLccxrw9jbzdEWFMQBcG8FD3.CDs07M0OZTks2IRY5Y-1701026738140-0-604800000");
+    myHeaders.append("Authorization", "Bearer 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");
 
     var raw = JSON.stringify({
     "model": "gpt-3.5-turbo",
@@ -57,10 +56,19 @@ function askOpenAI(prompt){
     redirect: 'follow'
     };
 
-    fetch("https://api.openai.com/v1/chat/completions", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    let result = "request failed.";
+    var response;
+    try
+    {
+        let response = await fetch("https://api.openai.com/v1/chat/completions", requestOptions);
+        let json = await response.json();
+        result = json.choices[0].message.content;
+    }
+    catch(error){
+        result = error;
+        console.log(error);
+    }
+    return result
 }
 
 
@@ -74,15 +82,15 @@ voice2TextButton.addEventListener("click", voice2Text);
 
 // TODO: Function that gives translatedText to the text2Speech API and streams audio
 function text2Speech() {
-    console.log("text2Speech")
+    console.log("text2Speech");
 }
 // TODO: Event Listener for translatedText, if has input run text2Speech
-translatedText.addEventListener("input", text2Speech);
+//translatedText.addEventListener("input", text2Speech);
 
 
 // TODO: Function that copies translatedText to clipboard
 function copyToClipBoard() {
-    console.log("copyToClipBoard")
+    console.log("copyToClipBoard");
 }
 // TODO: Event Listener for CopyToClipBoardButton
 CopyToClipBoardButton.addEventListener("click", copyToClipBoard);
